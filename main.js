e832418a72f2e2d5bf9c3d922411da2fd312fb0c
DaniilT048 <tis.daniil98@gmail.com>
2025-01-21 14:59:12 +0200

    ended question #3 bulo vajko

--- main.js
+++ main.js
@@ -41,13 +41,19 @@ console.log(fullName()) // John Smith
  *
  * Функція defUpperStr('My text') повертає текст, перетворений у верхній регістр, тобто: defUpperStr('My text') → 'MY TEXT'.
  *
- * Якщо функція викликається без параметра defUpperStr(), вона не повинна повертати undefined, у цьому випадку потрібно повернути рядок тексту за замовчуванням у верхньому регістрі, тобто defUpperStr() → 'DEFAULT TEXT'.
+ * Якщо функція викликається без параметра defUpperStr(), вона не повинна повертати undefined, у цьому випадку
+ * потрібно повернути рядок тексту за замовчуванням у верхньому регістрі, тобто defUpperStr() → 'DEFAULT TEXT'.
  *
  * При виконанні завдання не використовуйте оператор if, потрібен розв'язок із логічним оператором ||.
  */
 
-// console.log(defUpperStr('My text')) // MY TEXT
-// console.log(defUpperStr())             // DEFAULT TEXT
+function defUpperStr(text) {
+    return (text||'Default text').toUpperCase()
+}
+
+
+console.log(defUpperStr('My text')) // MY TEXT
+console.log(defUpperStr())             // DEFAULT TEXT
 
 /*
  * #4

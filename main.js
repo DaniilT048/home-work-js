01299ed22c6b2c35cebddb8fa6f3eff9f81a97c9
DaniilT048 <tis.daniil98@gmail.com>
2025-01-22 14:06:34 +0200

    #6 medium difficulty

--- main.js
+++ main.js
@@ -130,7 +130,6 @@ function weekFn(day) {
             break
         default:
             return null
-
     }
 }
 
@@ -159,25 +158,35 @@ console.log(weekFn('2')) // null
  * Використання операторів if, switch - заборонено.
  */
 
-// console.log('    -1 :', ageClassification(-1)) // -1 : null
-// console.log('     0 :', ageClassification(0)) // 0 : null
-// console.log('     1 :', ageClassification(1)) // 1 : Дитинство
-// console.log('    24 :', ageClassification(24)) // 24 : Дитинство
-// console.log(' 24.01 :', ageClassification(24.01)) // 24.01 : Молодість
-// console.log('    44 :', ageClassification(44)) // 44 : Молодість
-// console.log(' 44.01 :', ageClassification(44.01)) // 44.01 : Зрілість
-// console.log('    65 :', ageClassification(65)) // 65 : Зрілість
-// console.log('  65.1 :', ageClassification(65.1)) // 65.1 : Старість
-// console.log('    75 :', ageClassification(75)) // 75 : Старість
-// console.log(' 75.01 :', ageClassification(75.01)) // 75.01 : Довголіття
-// console.log('    90 :', ageClassification(90)) // 90 : Довголіття
-// console.log(' 90.01 :', ageClassification(90.01)) // 90.01 : Рекорд
-// console.log('   122 :', ageClassification(122)) // 122 : Рекорд
-// console.log('122.01 :', ageClassification(122.01)) // 122.01 : null
-// console.log('   150 :', ageClassification(150)) // 150 : null
+function ageClassification(age) {
+    return age > 0 && age <= 24 ? 'Дитинство' :
+        age > 24 && age <= 44 ? "Молодість" :
+            age > 44 && age <= 65 ? "Зрілість" :
+                age > 65 && age <= 75 ? "Старість" :
+                    age > 75 && age <= 90 ? "Довголіття" :
+                        age > 90 && age <= 122 ? "Рекорд" : null
+}
 
-/*
- Блок тестирования, везде должны быть true:
+console.log('    -1 :', ageClassification(-1)) // -1 : null
+console.log('     0 :', ageClassification(0)) // 0 : null
+console.log('     1 :', ageClassification(1)) // 1 : Дитинство
+console.log('    24 :', ageClassification(24)) // 24 : Дитинство
+console.log(' 24.01 :', ageClassification(24.01)) // 24.01 : Молодість
+console.log('    44 :', ageClassification(44)) // 44 : Молодість
+console.log(' 44.01 :', ageClassification(44.01)) // 44.01 : Зрілість
+console.log('    65 :', ageClassification(65)) // 65 : Зрілість
+console.log('  65.1 :', ageClassification(65.1)) // 65.1 : Старість
+console.log('    75 :', ageClassification(75)) // 75 : Старість
+console.log(' 75.01 :', ageClassification(75.01)) // 75.01 : Довголіття
+console.log('    90 :', ageClassification(90)) // 90 : Довголіття
+console.log(' 90.01 :', ageClassification(90.01)) // 90.01 : Рекорд
+console.log('   122 :', ageClassification(122)) // 122 : Рекорд
+console.log('122.01 :', ageClassification(122.01)) // 122.01 : null
+console.log('   150 :', ageClassification(150)) // 150 : null
+
+
+ console.log ('Блок тестирования, везде должны быть true:')
+
  console.log('    -1 :', ageClassification(-1) === null); // -1 : null
  console.log('     0 :', ageClassification(0) === null) // 0 : null
  console.log('     1 :', ageClassification(1) === 'Дитинство'); // 1 : Дитинство
@@ -194,7 +203,7 @@ console.log(weekFn('2')) // null
  console.log('   122 :', ageClassification(122) === 'Рекорд'); // 122 : Рекорд
  console.log('122.01 :', ageClassification(122.01) === null); // 122.01 : null
  console.log('   150 :', ageClassification(150) === null); // 150 : null
-*/
+
 
 /*
  * #7
